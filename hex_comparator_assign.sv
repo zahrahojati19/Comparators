8d8f94091f97fffab445d6479aa290fff8e08db0
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2023-05-23 18:50:22 +0330

    Add files via upload

---
+++ hex_comparator_assign.sv
@@ -0,0 +1,5 @@
+module hexcomp16bit(input [15:0]a,b,input l,e,g,output L,E,G);
+  assign {L,G,E}=    (a<b | (a==b & l==1))?3'b100:
+                     (a>b | (a==b & g==1))?3'b010:
+                     (a==b | e==1)?{l,g,e}:3'b001;
+endmodule
