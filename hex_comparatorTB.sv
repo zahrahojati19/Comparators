efed2385c0964abb34e0f22123d3501e4f77f9f9
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2023-05-23 18:49:48 +0330

    Add files via upload

---
+++ hex_comparatorTB.sv
@@ -0,0 +1,18 @@
+module hexcomparatorTB();
+  logic [15:0]a,b;
+  logic l,e,g;
+  wire L,E,G;
+  hexcomparator tb(.a(a),.b(b),.l(l),.e(e),.g(g),.L(L),.E(E),.G(G));
+  initial begin
+    $monitor(a,b,l,e,g,L,E,G);
+    a=15'b0;b=15'b0;l=0;e=0;g=0;#59
+    e=1;#59
+    e=0;#59
+    b[5]=1;#59
+    b[5]=0;#59
+    a[5]=1;#59
+    a[5]=0;
+  end
+endmodule
+
+
